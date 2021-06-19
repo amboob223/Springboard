55194d03113c3b60691b71454f6c7df8cc7b3bbf
amboob223 <67025021+amboob223@users.noreply.github.com>
2021-06-18 21:08:25 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
 
-btn.addEventListener("click", function(){// events listene tells the button to do something when a button is clicked
-    //get random nunmvber betwwen zero and 3 colors[3]
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber); 
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-});
+//set count tp zero  
+let count = 0;
 
+// select value and buttons 
+const value = document.querySelector("#value");// using the document object
+const btns = document.querySelectorAll(".btn"); // collecting all the buttons
 
-function getRandomNumber(){
-    return Math.floor(Math.random() * colors.length); 
-}
+console.log(btns);// these are for all three of them buttons
 
+btns.forEach(function(btn){ // we are iterating through th buttons that have the class btn in the html
+    btn.addEventListener("click",function(e){ // this is making the buttons do something when clicked // event object
+        const styles = e.currentTarget.classList; // making the styles varible // class list arranges all the btn classes nextt to btn 
+        if(styles.contains("decrease")){
+            count--;
+        } else if(styles.contains("increase")){
+             count++;
+        } else{
+            count = 0; // the last choice is reset and thats why it goes bak to setting the count to zero
+            
+        }
+        if(count > 0) {
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count == 0){
+            value.style.color = "black";
+        }
+        value.textContent = count;// we got to make the vcalue text conten t equal the count depeding on the click 
+    });
+
+}); 
