b12397099d36a8761d3b3d677e20d1aef41e4344
amboob223 <67025021+amboob223@users.noreply.github.com>
2021-06-23 12:43:16 -0700

    This is part of the nav js project to get toggle functionality

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
 
-//set count tp zero  
-let count = 0;
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-// select value and buttons 
-const value = document.querySelector("#value");// using the document object
-const btns = document.querySelectorAll(".btn"); // collecting all the buttons
-
-console.log(btns);// these are for all three of them buttons
-
-btns.forEach(function(btn){ // we are iterating through th buttons that have the class btn in the html
-    btn.addEventListener("click",function(e){ // this is making the buttons do something when clicked // event object
-        const styles = e.currentTarget.classList; // making the styles varible // class list arranges all the btn classes nextt to btn 
-        if(styles.contains("decrease")){
-            count--;
-        } else if(styles.contains("increase")){
-             count++;
-        } else{
-            count = 0; // the last choice is reset and thats why it goes bak to setting the count to zero
-            
-        }
-        if(count > 0) {
-            value.style.color = "green";
-        }
-        if(count < 0){
-            value.style.color = "red";
-        }
-        if(count == 0){
-            value.style.color = "black";
-        }
-        value.textContent = count;// we got to make the vcalue text conten t equal the count depeding on the click 
-    });
-
-}); 
+navToggle.addEventListener("click", function(){
+    links.classList.toggle("show-links");// if its got show links it does something and if not it shows link becuase of the togglr property 
+});
+    
+    
